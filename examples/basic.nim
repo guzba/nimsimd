675186056385685cee581cabfada23a8afd7c97e
Ryan Oldenburg <ryan@guzba.com>
2022-08-03 03:42:26 -0500

    rm

--- examples/basic.nim
+++ examples/basic.nim
@@ -2,9 +2,9 @@ import nimsimd/sse2
 
 # SIMD floating point multiplication
 let
-  a = m128(1.0) # Vector of 4 float32 each with value 1.0
-  b = m128(2.0) # Vector of 4 float32 each with value 2.0
-  c = a * b # SIMD vector multiplication operator
+  a = mm_set1_ps(1.0) # Vector of 4 float32 each with value 1.0
+  b = mm_set1_ps(2.0) # Vector of 4 float32 each with value 2.0
+  c = mm_mul_ps(a, b) # SIMD vector multiplication operator
 
 # Cast the vector to echo as separate float32 values
 echo cast[array[4, float32]](c)
--- tests/test.nim
+++ tests/test.nim
@@ -1,8 +1,8 @@
 import nimsimd/sse42
 
 let
-  a = m128(1.0)
-  b = m128(2.0)
-  c = a * b
+  a = mm_set1_ps(1.0)
+  b = mm_set1_ps(2.0)
+  c = mm_mul_ps(a, b)
 
 doAssert cast[array[4, float32]](c) == [2.0.float32, 2.0, 2.0, 2.0]
