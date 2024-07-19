3518f67330d0644f9bf9fda5a7c5462afdb6a2cb
Andreas Schoeller <andreas.schoeller@icloud.com>
2024-07-19 19:04:08 +0200

    Update movbe.nim
    fixed signatures in :
    - storebe_i16()
    - storebe_i32()
    - storebe_i64()

--- src/nimsimd/movbe.nim
+++ src/nimsimd/movbe.nim
@@ -1,12 +1,12 @@
 
 {.push header: "immintrin.h".}
 
-func loadbe_i16*(  pt :pointer ) :int16    {.importc: "_loadbe_i16".}
-func loadbe_i32*(  pt :pointer ) :int32    {.importc: "_loadbe_i32".}
-func loadbe_i64*(  pt :pointer ) :int64    {.importc: "_loadbe_i64".}
+func loadbe_i16*(  pt :pointer ) :int16       {.importc: "_loadbe_i16".}
+func loadbe_i32*(  pt :pointer ) :int32       {.importc: "_loadbe_i32".}
+func loadbe_i64*(  pt :pointer ) :int64       {.importc: "_loadbe_i64".}
 
-func storebe_i16*( pt :pointer ) :int16    {.importc: "_storebe_i16".}
-func storebe_i32*( pt :pointer ) :int32    {.importc: "_storebe_i32".}
-func storebe_i64*( pt :pointer ) :int64    {.importc: "_storebe_i64".}
+func storebe_i16*( pt :pointer, data :int16 ) {.importc: "_storebe_i16".}
+func storebe_i32*( pt :pointer, data :int32 ) {.importc: "_storebe_i32".}
+func storebe_i64*( pt :pointer, data :int64 ) {.importc: "_storebe_i64".}
 
 {.pop.}
