4e4fe5bd178d3e1567f4c1bf5780a0be66440628
Ryan Oldenburg <ryan@guzba.com>
2024-07-26 13:10:51 -0500

    fix signature

--- src/nimsimd/bmi2.nim
+++ src/nimsimd/bmi2.nim
@@ -3,8 +3,8 @@
 func bzhi_u32*(a: uint32, index: uint32): uint32 {.importc:"_bzhi_u32".}
 func bzhi_u64*(a: uint64, index: uint32): uint64 {.importc:"_bzhi_u64".}
 
-func mulx_u32*(a, b: uint32; hi: ptr uint32): uint32 {.importc:"_mulx_32".}
-func mulx_u64*(a, b: uint64; hi: ptr uint64): uint64 {.importc:"_mulx_64".}
+func mulx_u32*(a, b: uint32; hi: ptr uint32): uint32 {.importc:"_mulx_u32".}
+func mulx_u64*(a, b: uint64; hi: ptr uint64): uint64 {.importc:"_mulx_u64".}
 
 func pext_u32*(a, mask: uint32): uint32 {.importc:"_pext_u32".}
 func pext_u64*(a, mask: uint64): uint64 {.importc:"_pext_u64".}
