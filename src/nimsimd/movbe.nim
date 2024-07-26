2cdefccab88508ec644a17a964a85c3079890209
Ryan Oldenburg <ryan@guzba.com>
2024-07-26 12:58:23 -0500

    follow lib standard formatting

--- src/nimsimd/movbe.nim
+++ src/nimsimd/movbe.nim
@@ -1,12 +1,11 @@
-
 {.push header: "immintrin.h".}
 
-func loadbe_i16*(  pt :pointer ) :int16       {.importc: "_loadbe_i16".}
-func loadbe_i32*(  pt :pointer ) :int32       {.importc: "_loadbe_i32".}
-func loadbe_i64*(  pt :pointer ) :int64       {.importc: "_loadbe_i64".}
+func loadbe_i16*(p: pointer): int16 {.importc: "_loadbe_i16".}
+func loadbe_i32*(p: pointer): int32 {.importc: "_loadbe_i32".}
+func loadbe_i64*(p: pointer): int64 {.importc: "_loadbe_i64".}
 
-func storebe_i16*( pt :pointer, data :int16 ) {.importc: "_storebe_i16".}
-func storebe_i32*( pt :pointer, data :int32 ) {.importc: "_storebe_i32".}
-func storebe_i64*( pt :pointer, data :int64 ) {.importc: "_storebe_i64".}
+func storebe_i16*(p: pointer, data: int16) {.importc: "_storebe_i16".}
+func storebe_i32*(p: pointer, data: int32) {.importc: "_storebe_i32".}
+func storebe_i64*(p: pointer, data: int64) {.importc: "_storebe_i64".}
 
 {.pop.}
