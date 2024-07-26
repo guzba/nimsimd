b48c6d54bbcf39a7ca104f9d18880b28a6b0f594
Ryan Oldenburg <ryan@guzba.com>
2024-07-26 13:01:06 -0500

    follow lib standard formatting

--- src/nimsimd/f16c.nim
+++ src/nimsimd/f16c.nim
@@ -1,21 +1,19 @@
-
 import ./[sse2, avx]
-export sse2
-export avx
+export sse2, avx
 
 {.push header: "immintrin.h".}
 
 const
-  MM_FROUND_TO_NEAREST_INT* :int32 = 0 ## round to nearest
-  MM_FROUND_TO_NEG_INF*     :int32 = 1 ## round down
-  MM_FROUND_TO_POS_INF*     :int32 = 2 ## round up
-  MM_FROUND_TO_ZERO*        :int32 = 3 ## truncate
-  MM_FROUND_CUR_DIRECTION*  :int32 = 4 ## use MXCSR.RC; see _MM_SET_ROUNDING_MODE
+  MM_FROUND_TO_NEAREST_INT*: int32 = 0 ## round to nearest
+  MM_FROUND_TO_NEG_INF*: int32 = 1 ## round down
+  MM_FROUND_TO_POS_INF*: int32 = 2 ## round up
+  MM_FROUND_TO_ZERO*: int32 = 3 ## truncate
+  MM_FROUND_CUR_DIRECTION*: int32 = 4 ## use MXCSR.RC; see _MM_SET_ROUNDING_MODE
 
-func mm_cvtph_ps*(    vec :M128i )             :M128  {.importc: "_mm_cvtph_ps".}
-func mm_cvtps_ph*(    vec :M128, imm8 :int32 ) :M128i {.importc: "_mm_cvtps_ph".}
+func mm_cvtph_ps*(a: M128i ): M128 {.importc: "_mm_cvtph_ps".}
+func mm_cvtps_ph*(a: M128, imm8: int32): M128i {.importc: "_mm_cvtps_ph".}
 
-func mm256_cvtph_ps*( vec :M128i )             :M256  {.importc: "_mm256_cvtph_ps".}
-func mm256_cvtps_ph*( vec :M256, imm8 :int32 ) :M128i {.importc: "_mm256_cvtps_ph".}
+func mm256_cvtph_ps*(a: M128i): M256 {.importc: "_mm256_cvtph_ps".}
+func mm256_cvtps_ph*(a: M256, imm8: int32): M128i {.importc: "_mm256_cvtps_ph".}
 
 {.pop.}
